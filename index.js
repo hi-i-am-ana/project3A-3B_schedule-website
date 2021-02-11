bf790668c77496eb7eda5f1aaa8bb59510f5f27f
Anastasia Chen <anastasia@Anastasias-MacBook-Air.local>
2021-02-11 12:13:21 +1100

    add body-parsing middleware, change post routes to render html

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const crypto = require('crypto');
 const app = express();
 const PORT = process.env.port || 3000;
 
+// TODO: From express documentation (http://expressjs.com/en/4x/api.html#req.body) - research
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
 // Settings to use templates
 app.set('views', './views');
 app.set('view engine', 'pug');
@@ -38,32 +42,30 @@ app.get('/users/new', (req, res) => res.render('content_new_user'));
 app.get('/schedules/new', (req, res) => res.render('content_new_schedule', {users: data.users}));
 
 // Post new schedule
-// TODO: Or req.body???
 // TODO: Check if user doesn't exist?
 app.post('/schedules', (req, res) => {
     const newSchedule = {
-        'user_id': Number(req.query.user_id),
-        'day': Number(req.query.day),
-        'start_at': req.query.start_at,
-        'end_at': req.query.end_at
+        'user_id': Number(req.body.user_id),
+        'day': Number(req.body.day),
+        'start_at': req.body.start_at,
+        'end_at': req.body.end_at
     };
     data.schedules.push(newSchedule);
-    res.send(newSchedule);
+    res.render('content_schedules', {schedules: data.schedules});
 });
 
 // Post new user
 app.post('/users', (req, res) => {
-    // TODO: Research
-    const encriptedPassword = crypto.createHash('sha256').update(req.query.password).digest('base64');
-    console.log(encriptedPassword)
+    // TODO: Research https://nodejs.org/en/knowledge/cryptography/how-to-use-crypto-module/
+    const encriptedPassword = crypto.createHash('sha256').update(req.body.password).digest('base64');
     const newUser = {
-        'firstname': req.query.firstname,
-        'lastname': req.query.lastname,
-        'email': req.query.email,
+        'firstname': req.body.firstname,
+        'lastname': req.body.lastname,
+        'email': req.body.email,
         'password': encriptedPassword
     };
     data.users.push(newUser);
-    res.send(newUser);
+    res.render('content_users', {users: data.users});
 });
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`))
